c62696b6c4c7a7afbef7c44b9b0fefe7d502aaca
HossamElDin AbdElGhani ElKordi <haelkordi@ejadaalex.local>
2023-07-07 22:34:10 +0200

    Initial Commit

--- sparsercnn/backbone/feature_extractor.py
+++ sparsercnn/backbone/feature_extractor.py
@@ -3,6 +3,11 @@ from .vit import vit_base
 from .sfp import SimpleFeaturePyramid, LastLevelMaxPool
 from detectron2.modeling import BACKBONE_REGISTRY, Backbone, ShapeSpec
 
+def load_vit_model(vit_model, path, device):
+       ckpt = torch.load(path, map_location=device)
+       state_dict = {k.partition('module.')[2]: v for k,v in ckpt.items()}
+       vit_model.load_state_dict(state_dict) 
+
 @BACKBONE_REGISTRY.register()
 class FeatureExtractor(Backbone):
     def __init__(self, cfg, input_shape):
@@ -11,10 +16,10 @@ class FeatureExtractor(Backbone):
         """
 
         super().__init__()
-        self.vit_model = vit_base(img_size=[448])
-        self.load_vit_model(path='/content/drive/MyDrive/target_encoder.pth.tar', device=cfg.MODEL.DEVICE)
+        vit_model = vit_base(img_size=[448])
+        load_vit_model(vit_model, path='/content/drive/MyDrive/target_encoder.pth.tar', device=cfg.MODEL.DEVICE)
         self.model_backbone = SimpleFeaturePyramid(
-            net=self.vit_model,
+            net=vit_model,
             in_feature="last_feat",
             out_channels=256,
             scale_factors=[4.0,2.0,1.0,0.5],
@@ -25,11 +30,6 @@ class FeatureExtractor(Backbone):
 
     def forward(self, image):
         return self.model_backbone(image)
-
-    def load_vit_model(self, path, device):
-       ckpt = torch.load(path, map_location=device)
-       state_dict = {k.partition('module.')[2]: v for k,v in ckpt.items()}
-       self.vit_model.load_state_dict(state_dict) 
 
     def output_shape(self):
         return self.model_backbone.output_shape()
